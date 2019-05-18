7ac8fe9c819ba8ddc4fd86d9b6973692da813024
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2019-05-17 21:56:26 -0300

    2019/05/17 21:56 better var names

--- sortplot.cpp
+++ sortplot.cpp
@@ -18,33 +18,33 @@ sortplot::~sortplot(){
 }
 
 void sortplot::on_sortButton_clicked(){
-	int min,aux;//variavéis unicamente auxíliares
+	int minim,aux;//variavéis unicamente auxíliares
 	int comp,swi;//contador de comparações e trocas realizadas
 	//<selection sort>
 	ui->comp->display(0);
 	ui->swi->display(0);
 	if(ui->sorting->currentText() == "Selection sort"){
-		min=0,aux=0,comp=0,swi=0;
+		minim=0,aux=0,comp=0,swi=0;
 		for (int i = 0; i < qv_x.size(); i++) {
-			min = i;
+			minim = i;
 			for(int j =i+1;j< qv_x.size();j++){
 				ui->comp->display(++comp);
 				replotbars(i,j);
-				if(qv_y[j]<qv_y[min])
-					min = j;
+				if(qv_y[j]<qv_y[minim])
+					minim = j;
 			}
-			if(qv_y[i] != qv_y[min]){
+			if(qv_y[i] != qv_y[minim]){
 				ui->swi->display(++swi);
 				aux = qv_y[i];
-				qv_y[i] = qv_y[min];
-				qv_y[min] = aux;
+				qv_y[i] = qv_y[minim];
+				qv_y[minim] = aux;
 			}
 			
 		}
 	}
 	//<bubble sort>
 	if(ui->sorting->currentText() == "Bubble sort"){
-		min=0,aux=0,comp=0,swi=0;
+		minim=0,aux=0,comp=0,swi=0;
 		for (int i = 1; i < qv_x.size(); i++) {
 			for (int j = 0; j < qv_x.size() -i ; j++) {
 				ui->comp->display(++comp);
@@ -87,16 +87,16 @@ void sortplot::on_unsortButton_clicked(){
 	unsort();
 }
 
-void sortplot::replotbars(int min,int key){
+void sortplot::replotbars(int minim,int key){
 	
 	QCPBars* nbar;
 	QCPBars* keybar;
-	QCPBars* minbar;
+	QCPBars* minimbar;
 	
 	ui->plot->clearPlottables();
 	
 	nbar = new QCPBars(ui->plot->xAxis, ui->plot->yAxis);
-	minbar = new QCPBars(ui->plot->xAxis, ui->plot->yAxis);
+	minimbar = new QCPBars(ui->plot->xAxis, ui->plot->yAxis);
 	keybar = new QCPBars(ui->plot->xAxis, ui->plot->yAxis);
 	
 	nbar->setStackingGap(0);
@@ -104,8 +104,8 @@ void sortplot::replotbars(int min,int key){
 	nbar->setBrush(QColor(111, 9, 176));
 	nbar->setData(qv_x,qv_y);
 	
-	minbar->setPen(QColor(255, 0, 70, 255));
-        minbar->addData(min,1001);
+	minimbar->setPen(QColor(255, 0, 70, 255));
+        minimbar->addData(minim,1001);
 	
 	keybar->setPen(QColor(0,255,255));
 	keybar->addData(key,1001);
